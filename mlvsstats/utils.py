00df0fe7246716f5c4c7651d7fd7a8da34fbebbe
Yousef <yousefammarabdullah.a@gmail.com>
2025-09-15 23:34:35 +0300

    first commit

--- mlvsstats/utils.py
+++ mlvsstats/utils.py
@@ -1,20 +1,25 @@
 import json
 import os
+import pathlib
 from joblib import dump, load
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-def load_config():
+
+def load_config(config_path: str = "configs/config.json") -> dict:
     """Load configuration from JSON file."""
-    with open("configs/config.json", "r") as f:
+    with open(config_path, "r") as f:
         config = json.load(f)
     return config
 
-def ensure_dir(dir_path):
+def ensure_dir(full_path: str ):
     """Ensure directory exists."""
-    if not os.path.exists(dir_path):
+    dir_path = os.path.dirname(full_path)
+    if dir_path:
         os.makedirs(dir_path, exist_ok=True)
 
-def save_artifacts(obj, dir_path):
-    """Save artifacts."""
-    ensure_dir(dir_path)
-    
+def save_artifact(obj: object, full_path: str = "artifact.joblib"):
+    """Save artifact."""
+    ensure_dir(full_path)
+    dump(obj, pathlib.Path(full_path))
+
+def load_artifact(full_path: str = "artifact.joblib") -> object:
+    """Load artifact."""
+    return load(pathlib.Path(full_path))
